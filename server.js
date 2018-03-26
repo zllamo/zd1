41cfd50b00c60cae91bde1421ff26c0658b8b11b
Zakary H Llamo-Cohen <zak.llamocohen@gmail.com>
2018-03-26 17:43:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function queryDatabase()
 
        // Read all rows from table
      request = new Request(
-          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+          "SELECT TOP 5 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
              function(err, rowCount, rows) 
                 {
                     console.log(rowCount + ' row(s) returned');
