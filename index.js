a55f5e75dd19152ddc382d3e5b3c38101fdd4e69
Zakary H Llamo-Cohen <zak.llamocohen@gmail.com>
2018-03-27 12:32:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
+var http = require('http');
 
-// Create connection to database
-var config = 
-   {
-     userName: 'zllamo', // update me
-     password: 'Walton!3', // update me
-     server: 'zd1.database.windows.net', // update me
-     options: 
-        {
-           database: 'zd1Test' //update me
-           , encrypt: true
-        }
-   }
-var connection = new Connection(config);
+var server = http.createServer(function(request, response) {
 
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) 
-   {
-     if (err) 
-       {
-          console.log(err)
-       }
-    else
-       {
-           queryDatabase()
-       }
-   }
- );
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-function queryDatabase()
-   { console.log('Reading rows from the Table...');
+});
 
-       // Read all rows from table
-     request = new Request(
-          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
-             function(err, rowCount, rows) 
-                {
-                    console.log(rowCount + ' row(s) returned');
-                    process.exit();
-                }
-            );
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-     request.on('row', function(columns) {
-        columns.forEach(function(column) {
-            console.log("%s\t%s", column.metadata.colName, column.value);
-         });
-             });
-     connection.execSql(request);
-   }
+console.log("Server running at http://localhost:%d", port);
