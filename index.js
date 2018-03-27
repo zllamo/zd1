af542f9ddff88f4024d84d9c03cd51327ca22e89
Zakary H Llamo-Cohen <zak.llamocohen@gmail.com>
2018-03-27 12:15:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,38 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
+var Connection = require('tedious').Connection;  
+    var config = {  
+        userName: 'zllamo',  
+        password: 'Walton!3',  
+        server: 'zd1.database.windows.net',  
+        // If you are on Azure SQL Database, you need these next options.  
+        options: {encrypt: true, database: 'AdventureWorks'}  
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement1();  
+    });  
 
-// Create connection to database
-var config = 
-   {
-     userName: 'zllamo', // update me
-     password: 'Walton!3', // update me
-     server: 'zd1.database.windows.net', // update me
-     options: 
-        {
-           database: 'zd1Test' //update me
-           , encrypt: true
-        }
-   }
-var connection = new Connection(config);
+    var Request = require('tedious').Request  
+    var TYPES = require('tedious').TYPES;  
 
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) 
-   {
-     if (err) 
-       {
-          console.log(err)
-       }
-    else
-       {
-           queryDatabase()
-       }
-   }
- );
-
-function queryDatabase()
-   { console.log('Reading rows from the Table...');
-
-       // Read all rows from table
-     request = new Request(
-          "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
-             function(err, rowCount, rows) 
-                {
-                    console.log(rowCount + ' row(s) returned');
-                    process.exit();
-                }
-            );
-
-     request.on('row', function(columns) {
-        columns.forEach(function(column) {
-            console.log("%s\t%s", column.metadata.colName, column.value);
-         });
-             });
-     connection.execSql(request);
-   }
+    function executeStatement1() {  
+        request = new Request("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {  
+         if (err) {  
+            console.log(err);}  
+        });  
+        request.addParameter('Name', TYPES.NVarChar,'SQL Server Express 2014');  
+        request.addParameter('Number', TYPES.NVarChar , 'SQLEXPRESS2014');  
+        request.addParameter('Cost', TYPES.Int, 11);  
+        request.addParameter('Price', TYPES.Int,11);  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                console.log("Product id of inserted item is " + column.value);  
+              }  
+            });  
+        });       
+        connection.execSql(request);  
+    }
